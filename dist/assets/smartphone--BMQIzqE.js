3c408d72a011ab61fb737a909e016cc2013149c5
bilaal5279 <bilaal5279@gmail.com>
2026-08-22 17:09:54 +0100

    Document TipMint data deletion requests

---
+++ dist/assets/smartphone--BMQIzqE.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-Bgf3iKlW.js";var t=e(`Smartphone`,[[`rect`,{width:`14`,height:`20`,x:`5`,y:`2`,rx:`2`,ry:`2`,key:`1yt0o3`}],[`path`,{d:`M12 18h.01`,key:`mhygvu`}]]);export{t};
